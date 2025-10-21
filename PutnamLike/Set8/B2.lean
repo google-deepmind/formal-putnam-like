63f58e777a7a2a14fee17114d02a40536f984426
Paul Lezeau <lezeau@google.com>
2025-10-21 02:28:22 -0700

    Add formalisation of putnam_like_set8_b2
    PiperOrigin-RevId: 822020430
    Change-Id: I9142b4d893ad3fd29d02ff7509ff7c8827447800

--- PutnamLike/Set8/B2.lean
+++ PutnamLike/Set8/B2.lean
@@ -16,10 +16,27 @@ limitations under the License.
 
 import Mathlib
 
+/-- Solution: \frac{2025}{8096}. -/
+abbrev putnam_like_set8_b2_solution : ℚ := sorry
+-- 2025 / 8096
+
 /--
 Given 2025 boxes numbered 0, 1, $\ldots$ 2024. Each $k$-th box contains $k$ white and $2024-k$ black
 balls. Maria chooses randomly one box and draws 3 balls (with returning). What is the probability
 that all of them are white?
 -/
-theorem putnam_like_set8_b2 : True := by
+theorem putnam_like_set8_b2
+    -- `IsWhiteBallFor k i` is the predicate that the `i`th ball in box `k` is white.
+    (IsWhiteBallFor : Fin 2025 → Fin 2024 → Prop)
+    [∀ box, DecidablePred <| IsWhiteBallFor box]
+    -- The `k`th box contains `k` white balls (i.e. there are `k` elements of `Fin 2024`
+    -- that satisfy `IsWhiteBallFor k` )
+    (IsWhiteBallFor_pred : ∀ boxIdx,
+      (Finset.univ.filter (IsWhiteBallFor boxIdx)).card = boxIdx) :
+    {(box, mariaChoice) : Fin 2025 × Multiset (Fin 2024) |
+      mariaChoice.card = 3 ∧
+      ∀ ball ∈ mariaChoice, IsWhiteBallFor box ball}.ncard /
+    {(box, mariaChoice) : Fin 2025 × Multiset (Fin 2024) |
+      mariaChoice.card = 3}.ncard
+      = putnam_like_set8_b2_solution := by
   sorry
